78ab2e35fc07340b5c9ba2ad0f2e57a88510a978
ashish3462 <Dahiya7372@975219P>
2024-09-01 11:34:36 +0530

    Third

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,13 @@
+// // src/api/apiConfig.js
+
+// export const API_URLS = {
+//   GENERATE_QR_CODE: "http://localhost:9899/api/generate",
+//   SCAN_DATA: "http://localhost:9899/api/scanned-data",
+// };
+
 // src/api/apiConfig.js
 
 export const API_URLS = {
-  GENERATE_QR_CODE: "http://localhost:9899/api/generate",
-  SCAN_DATA: "http://localhost:9899/api/scanned-data",
+  GENERATE_QR_CODE: "bestqrcodebackendspringboot.railway.internal/api/generate",
+  SCAN_DATA: "bestqrcodebackendspringboot.railway.internal/api/scanned-data",
 };
