625ec5bdc4f9386024c021175a9ba87906c26fcb
ashish3462 <Dahiya7372@975219P>
2024-09-01 12:47:18 +0530

    Five

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -8,8 +8,6 @@
 // src/api/apiConfig.js
 
 export const API_URLS = {
-  GENERATE_QR_CODE:
-    "https://bestqrcodebackendspringboot.railway.internal/api/generate",
-  SCAN_DATA:
-    "https://bestqrcodebackendspringboot.railway.internal/api/scanned-data",
+  GENERATE_QR_CODE: "bestqrcodebackendspringboot.railway.internal/api/generate",
+  SCAN_DATA: "bestqrcodebackendspringboot.railway.internal/api/scanned-data",
 };
